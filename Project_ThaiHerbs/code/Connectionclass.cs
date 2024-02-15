359fc304f4b4c7a4f1b3b90feeb3115d69a2ccda
earth <earth@EARTHSR>
2024-02-15 20:21:18 +0700

    Add project files.

---
+++ Project_ThaiHerbs/code/Connectionclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Timers;
+using System.Text;
+/// <summary>
+/// Summary description for Connectionclass
+/// </summary>
+public class Connectionclass
+{
+    private static SqlConnection conn;
+    private static SqlCommand command;
+    public Connectionclass()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["dbWebCSCPCConnectionString"].ToString();
+        conn = new SqlConnection(connectionString);
+        command = new SqlCommand("", conn);
+    }
+}
